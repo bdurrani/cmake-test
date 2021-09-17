aee8c41c7bdb4f3b8a9ee90e6dfc4c4d95e4a41d
Bilal Durrani <bilal33@gmail.com>
2021-09-17 14:18:52 -0400

    Add versioning

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "adder.h"
 #include "GLFW/glfw3.h"
+#include "cmaketestConfig.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
   std::cout << "Hello cmake";
   cout << adder(1, 3.0) << endl;
+  cout << argv[0] << " Version " << cmaketest_VERSION_MAJOR << "." << cmaketest_VERSION_MINOR << endl;
+
   GLFWwindow *window;
   glfwInit();
 }
